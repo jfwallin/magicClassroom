184a5c3dbdce238245523cd9ff493ce152f15ed0
Sharkful <53191311+Sharkful@users.noreply.github.com>
2019-12-18 13:03:54 -0600

    another push

---
+++ ishirk/UnityProjects/Duel Concept/Assets/LauncherScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherScript : MonoBehaviour
+{
+    public Vector3 LaunchLocation;
+    public GameObject projectilePrefab;
+    public float triggerDistance;
+
+    private GameObject cameraObject;
+    private bool triggered;
+
+    void Start()
+    {
+        cameraObject = Camera.main.gameObject;
+    }
+
+    void Update()
+    {
+        if ((cameraObject.transform.position - transform.position).sqrMagnitude < (triggerDistance*triggerDistance))
+        {
+            transform.LookAt(cameraObject.transform);
+            if(triggered == false)
+            {
+                triggered = true;
+                StartCoroutine(LaunchPrefab());
+            }
+        }
+        else
+        {
+            triggered = false;
+        }
+    }
+
+    private IEnumerator LaunchPrefab()
+    {
+        yield return null;
+    }
+
+}
