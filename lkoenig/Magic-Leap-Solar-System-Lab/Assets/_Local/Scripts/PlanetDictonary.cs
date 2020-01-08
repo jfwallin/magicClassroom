f4f2172f65220539e6e1a5fafb0a63063b7fc6f1
lak3r <35858548+lak3r@users.noreply.github.com>
2020-01-08 12:14:18 -0600

    This is it

---
+++ lkoenig/Magic-Leap-Solar-System-Lab/Assets/_Local/Scripts/PlanetDictonary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*This will store the info needed about the planets
+ * ie orbits/rotations/materials used(hopefully)/size/distance/ect
+ * In theory orbits and rotations ahould be properly proportioned and then it will be scaled in the TimeManager
+ * It will be based on the fact that FixedUpdate has a fixed time
+ * And that Earth will rotate 
+ * 
+ * This also maybe shouldn't be a MonoBehaviour but I'll deal with that later
+ */
+public class PlanetDictonary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
