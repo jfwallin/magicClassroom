133fbe8c08577ba1a60b29ef6653d029084141b0
Sharkful <53191311+Sharkful@users.noreply.github.com>
2020-02-12 16:51:17 -0600

    UI update
    finalized headpose ui for the opening phase of the application. Also added some tools to reset the menus to their proper initilizations with a keyboarf shortcut.

---
+++ ishirk/UnityProjects/Pong-Transmission/Assets/Scripts/ConnectingGUIManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using MagicLeapTools;
+
+/// <summary>
+/// Manages the display of multiplayer connection screen
+/// </summary>
+public class ConnectingGUIManager : MonoBehaviour
+{
+    //UI Objects to manipulate while connecting
+    [Header("UI Objects")]
+    [SerializeField]
+    private Text waitingText = null;
+    [SerializeField]
+    private Text connectedText = null;
+
+    private void Awake()
+    {
+        //Check references
+        Assert.IsNotNull(waitingText, "waitingText not assigned");
+        Assert.IsNotNull(connectedText, "connectedText not assigned");
+    }
+
+    private void OnEnable()
+    {
+        //Set default state, register event handler
+        waitingText.gameObject.SetActive(true);
+        connectedText.gameObject.SetActive(false);
+        Transmission.Instance.OnPeerFound.AddListener(handleOnPeerFound);
+
+        //check if the connection already occurred
+        if (Transmission.Peers.Length > 0)
+            handleOnPeerFound(Transmission.Peers[0]);
+    }
+
+    private void OnDisable()
+    {
+        //unregister event handler
+        Transmission.Instance.OnPeerFound.RemoveListener(handleOnPeerFound);
+    }
+
+    /// <summary>
+    /// Responds to transmissionconnecting to another app
+    /// </summary>
+    /// <param name="peerLabel">name of peer</param>
+    public void handleOnPeerFound(string peerLabel)
+    {
+        StartCoroutine(connectedTransition());
+    }
+
+    /// <summary>
+    /// Briefly displays connection confirmation
+    /// </summary>
+    private IEnumerator connectedTransition()
+    {
+        //display connectedText
+        waitingText.gameObject.SetActive(false);
+        connectedText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+
+        //Turn off. ADD CALLS TO SCENE MANAGER TO CHANGE STATE
+        gameObject.SetActive(false);
+        yield return null;
+    }
+}
---
+++ ishirk/UnityProjects/Pong-Transmission/Assets/Scripts/DevMenuTools.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DevMenuTools
+{
+    //[MenuItem("Tools/Initialize Scene")]
+    //private static void InitializeScene()
+    //{
+    //    Debug.Log("Initializing");
+    //    ResetHeadposeMenus();
+
+    //}
+
+    [MenuItem("Tools/Reset Headpose Menus &m")]
+    private static void ResetHeadposeMenus()
+    {
+        Debug.Log("Reseting headpose menus");
+        GameObject headposeUIParent = GameObject.Find("HeadposeCanvas");
+        if(!headposeUIParent)
+        {
+            Debug.LogWarning("couldn't find HeadposeCanvas");
+            return;
+        }
+        foreach(Transform phase in headposeUIParent.transform)
+        {
+            if(phase.name == "OpeningMenus")
+            {
+                phase.gameObject.SetActive(true);
+                foreach(Transform page in phase)
+                {
+                    if (page.name == "WelcomePage")
+                    {
+                        page.gameObject.SetActive(true);
+                        Debug.Log("Set WelcomePage active");
+                    }
+                    else
+                        page.gameObject.SetActive(false);
+                }
+            }
+            else
+                phase.gameObject.SetActive(false);
+        }
+    }
+
+    [MenuItem("Tools/Call On Handle Peer Found &p")]
+    private static void OnHandlePeerFound()
+    {
+        Debug.Log("Faking finding a peer");
+        ConnectingGUIManager manager = GameObject.Find("ConnectingPage")?.GetComponent<ConnectingGUIManager>();
+        if (manager != null)
+        {
+            manager.handleOnPeerFound("somePeer");
+            Debug.Log("Peer Faked");
+        }
+        else
+            Debug.LogWarning("Could not find ConnectingGUIManager");
+    }
+}
