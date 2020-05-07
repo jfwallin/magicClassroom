c08f2dc68dc9d4e19b1b6b9fc44a8166703aeea2
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2020-05-07 14:18:02 -0500

    Json testing
    I made a test scene with two test scripts. One is a serializable script and the seccond assigns things and prints it to the console.
    
    It's not doing exactly as expected but that might be because I wasn't running it in the simulator. It still kinda did as expected, but only for one pass.

---
+++ lkoenig/JSON test/Assets/_Local/Scripts/JSONTest0Logic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+
+public class JSONTest0Logic : MonoBehaviour
+{
+    private JSONTest0 test = new JSONTest0();
+    private string json;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        test.num = 13;
+        test.name = "Laurel Koenig";
+
+        json = JsonUtility.ToJson(test);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(json);
+    }
+}
