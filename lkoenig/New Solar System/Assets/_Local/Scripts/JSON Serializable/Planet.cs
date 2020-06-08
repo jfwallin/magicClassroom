3571876e67776baf645b817d1c29d2cdc65ada1f
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2020-06-08 17:39:05 -0500

    Making a make json script
    Some trouble with array but everything else working

---
+++ lkoenig/New Solar System/Assets/_Local/Scripts/JSON Serializable/Planet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Planet
+{
+    public string name;
+
+    public float xPosition;
+    public float yPosition;
+    public float zPosition;
+
+    public int numScriptsToAdd;
+    public string []scriptName;
+}
---
+++ lkoenig/New Solar System/Assets/_Local/Scripts/MakeJson.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MakeJson : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Planet planet = new Planet();
+        string json;
+        string path = "Assets/_Local/JSON Files/Earth.json";
+
+        StreamWriter writer = new StreamWriter(path);
+
+        planet.name = "Earth";
+        planet.xPosition = 0f;
+        planet.yPosition = 0f;
+        planet.zPosition = 0f;
+        planet.numScriptsToAdd = 3;
+        //planet.scriptName[0] = "Orbit";
+        //planet.scriptName[1] = "Rotate";
+        //planet.scriptName[2] = "Transmission Object";
+
+        json = JsonUtility.ToJson(planet);
+        Debug.Log("json file is: " + json);
+        writer.WriteLine(json);
+
+        writer.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
