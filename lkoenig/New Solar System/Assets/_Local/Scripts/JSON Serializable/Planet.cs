615427fa63d586f9ca197f21c20c329385cd55fb
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2020-06-08 18:18:50 -0500

    Made some JSON files
    For the Earth, Moon, and Sun specifically

--- lkoenig/New Solar System/Assets/_Local/Scripts/JSON Serializable/Planet.cs
+++ lkoenig/New Solar System/Assets/_Local/Scripts/JSON Serializable/Planet.cs
@@ -10,7 +10,16 @@ public class Planet
     public float xPosition;
     public float yPosition;
     public float zPosition;
+    public float scale;
+    public string material;
 
     public int numScriptsToAdd;
     public string []scriptName;
+
+    //Variable related to the Orbit Script
+    public string OrbitCenter;
+    public float OrbitRotateDegree;
+
+    //Variables related to the rotate script
+    public Vector3 RotateRotationAngle;
 }
--- lkoenig/New Solar System/Assets/_Local/Scripts/MakeJson.cs
+++ lkoenig/New Solar System/Assets/_Local/Scripts/MakeJson.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Collections.Specialized;
 
 public class MakeJson : MonoBehaviour
 {
@@ -10,19 +11,26 @@ public class MakeJson : MonoBehaviour
     {
         Planet planet = new Planet();
         string json;
-        string path = "Assets/_Local/JSON Files/Earth.json";
+        string path = "Assets/_Local/JSON Files/Sun.json";
 
         StreamWriter writer = new StreamWriter(path);
 
-        planet.name = "Earth";
+        planet.name = "Sun";
         planet.xPosition = 0f;
         planet.yPosition = 0f;
         planet.zPosition = 0f;
-        planet.numScriptsToAdd = 3;
+        planet.scale = 1f;
+        planet.material = "Sun";
+        planet.numScriptsToAdd = 2;
         planet.scriptName = new string[planet.numScriptsToAdd];
+
         planet.scriptName[0] = "Orbit";
+        //planet.OrbitCenter = "Earth";
+        planet.OrbitRotateDegree = 0f;
+
         planet.scriptName[1] = "Rotate";
-        planet.scriptName[2] = "Transmission Object";
+        planet.RotateRotationAngle = new Vector3(0,2.942379f,0);
+
 
         json = JsonUtility.ToJson(planet);
         Debug.Log("json file is: " + json);
