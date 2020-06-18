d19066b21525e38d33406e596f36fe43ed783cda
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2020-06-18 01:53:25 -0500

    Maker script working

--- lkoenig/JSON test/Assets/_Local/JSON Bridge/Scripts/JSON Serializable/MakeObjectInfo.cs
+++ lkoenig/JSON test/Assets/_Local/JSON Bridge/Scripts/JSON Serializable/MakeObjectInfo.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 
 //Due to the nature of ObjectInfo at this moment this also has to be updated every time a new JsonObject script is added
 public class MakeObjectInfo : MonoBehaviour
 {
+    public string path;
     public string name;
 
     public Vector3 position;
@@ -18,7 +20,28 @@ public class MakeObjectInfo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ObjectInfo info = new ObjectInfo();
+        string json;
+        path = path + name + ".json";
+
+        StreamWriter writer = new StreamWriter(path);
+
+        //Varriables
+        info.name = name;
+
+        info.position = position;
+        info.scale = scale;
+        info.material = material;
+
+        info.numScriptsToAdd = numScriptsToAdd;
+        info.scriptsToAdd = scriptsToAdd;
+        //end variables
+
+        json = JsonUtility.ToJson(info);
+        Debug.Log("json file is: " + json);
+        writer.WriteLine(json);
+
+        writer.Close();
     }
 
     
