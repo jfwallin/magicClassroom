4833d8caa1595f4cc8d4b8648b02d29dfdd30368
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2020-06-23 17:41:40 -0500

    Revert "It keeps renaming it againt my will"
    This reverts commit 4363110548985d798043a3eb5ea2bd1a8f683c38.

--- lkoenig/JSON test/Assets/_Local/JSON Bridge/Scripts/Bridge.cs
+++ lkoenig/JSON test/Assets/_Local/JSON Bridge/Scripts/Bridge.cs
@@ -13,8 +13,20 @@ using System.Runtime.Versioning;
 //I may rename this and change the way it's funtioning.
 
 
-public class Bridge: MonoBehaviour
+public class JsonSceneCreation : MonoBehaviour
 {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
     private ObjectInfo getInfo(string path)
     {
