45cb2511c22266660c238c4da8ff4cf6341e1297
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2020-07-03 13:19:22 -0500

    Revert "Merge pull request #16 from jfwallin/Bridge--lanes-closed-for-contruction"
    This reverts commit 71c4a9e431aa91c629f7de35f713b10816498c8b, reversing
    changes made to d31e33b0501f32fd0b9a05ee28e17c2e54d87cec.

--- lkoenig/JSON test/Assets/JSON Bridge/Scripts/MakeObjectInfo.cs
+++ lkoenig/JSON test/Assets/JSON Bridge/Scripts/MakeObjectInfo.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-using BifrostBridge;//Allows us to use Bridge.methods and other things related to the bridge
-
 
 //This script allows you to test the Bridge without a outside source.
 //You can create the JSON in the inspector and then test if it is working corrrectly in one run in the Unity Editor.
 public class MakeObjectInfo : MonoBehaviour
 {
+    private Bridge bridge = new Bridge();
+
     public string path; //Where is/will your json be located?
     public ObjectInfo info;
 
@@ -21,14 +21,14 @@ public class MakeObjectInfo : MonoBehaviour
 
         StreamWriter writer = new StreamWriter(path);
         
-        json = JsonUtility.ToJson(info, true); //Uses Unity's built in Json serializer
+        json = JsonUtility.ToJson(info, true);
         Debug.Log("json file is: " + json);
 
         writer.WriteLine(json);
 
         writer.Close();
 
-        Bridge.ParseJson(path); //calls the ParseJson method of the bridge which will in turn call the private methods to actually properly parse the JSON
+        bridge.ParseJson(path);
     }
 
     
