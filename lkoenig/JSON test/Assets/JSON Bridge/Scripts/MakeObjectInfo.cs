1e5dbaa8b2a9e2d0b91caaedf12cb7edaa8f50de
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2020-07-03 13:23:49 -0500

    Revert "Namespaces are cool and so am I"
    This reverts commit ff3186bbfc8822bb083bc8ab383142d0b0ec6cd4.

--- lkoenig/JSON test/Assets/JSON Bridge/Scripts/MakeObjectInfo.cs
+++ lkoenig/JSON test/Assets/JSON Bridge/Scripts/MakeObjectInfo.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-using BifrostBridge;//Allows us to use Bridge.methods and other things related to the bridge
-
 
 //This script allows you to test the Bridge without a outside source.
 //You can create the JSON in the inspector and then test if it is working corrrectly in one run in the Unity Editor.
 public class MakeObjectInfo : MonoBehaviour
 {
+    private Bridge bridge = new Bridge();
+
     public string path; //Where is/will your json be located?
     public ObjectInfo info;
 
@@ -28,7 +28,7 @@ public class MakeObjectInfo : MonoBehaviour
 
         writer.Close();
 
-        Bridge.ParseJson(path); //calls the ParseJson method of the bridge which will in turn call the private methods to actually properly parse the JSON
+        bridge.ParseJson(path);
     }
 
     
